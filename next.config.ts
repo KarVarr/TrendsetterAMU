6adb644b3138d548a4ee171a9e9ba4d801ccac95
KarVarr <megatr9n@gmail.com>
2025-03-30 14:42:53 +0400

    Update: Fix i18n build error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
+// import type { NextConfig } from "next";
+// import { i18n } from './next-i18next.config';
+
+// const nextConfig: NextConfig = {
+//   i18n,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
-import { i18n } from './next-i18next.config';
 
 const nextConfig: NextConfig = {
-  i18n,
+  // другие настройки (если они есть)
 };
 
 export default nextConfig;
-
